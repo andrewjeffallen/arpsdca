0a4264eb4cd288cd448941eeaba8b3165e1cf030
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-01-11 16:00:27 -0700

    Update dca.py

--- arps/dca.py
+++ arps/dca.py
@@ -13,7 +13,7 @@ def hyperbolic_equation(t, qi, b, di):
     return qi/((1.0+b*di*t)**(1.0/b))
     
     
-  def get_min_or_max_value_in_column_by_group(dataframe, group_by_column, calc_column, calc_type):
+def get_min_or_max_value_in_column_by_group(dataframe, group_by_column, calc_column, calc_type):
     """
     This function obtains the min or max value for a column, with a group by applied. For example,
     it could return the earliest (min) RecordDate for each API number in a dataframe 
@@ -29,7 +29,7 @@ def hyperbolic_equation(t, qi, b, di):
     value=dataframe.groupby(group_by_column)[calc_column].transform(calc_type)
     return value
     
-  def get_max_initial_production(df, number_first_days, variable_column, date_column):
+def get_max_initial_production(df, number_first_days, variable_column, date_column):
     """
    
     Arguments:
@@ -54,11 +54,16 @@ def hyperbolic_equation(t, qi, b, di):
     # create new datafram with values sorted
     return df_beginning_production[variable_column].max()
     
-  def plot_actual_vs_predicted_by_equations(df, x_variable, y_variables, plot_title):
+def plot_actual_vs_predicted_by_equations(df, x_variable, y_variables, plot_title):
     """
-Plot Results of graphs
+    Plot Results of graphs
     """
     df.plot(x=x_variable, y=y_variables, title=plot_title)
     plt.show()
     
-    
+def remove_nan_and_zeroes_from_columns(df, variable):
+    """
+   remove nans and nulls
+    """
+    filtered_df = df[(df[variable].notnull()) & (df[variable]>0)]
+    return filtered_df
