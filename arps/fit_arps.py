4f484f028b2fbcbb5f32db5c503bd7ab744af758
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-01-15 14:37:14 -0700

    Update fit_arps.py

--- arps/fit_arps.py
+++ arps/fit_arps.py
@@ -1,9 +1,9 @@
 import pandas 
-import numpy
+import numpy as np
 
 
 from scipy.optimize import curve_fit
-from dca import (
+from arps.dca import (
       hyperbolic_equation, 
       get_min_or_max_value_in_column_by_group,
       get_max_initial_production,
@@ -33,18 +33,34 @@ def execute_arps(unique_well_APIs_list,prd_time_series, days,liquid, b_upper,  d
         popt_hyp, pcov_hyp=curve_fit(hyperbolic_equation, production_time_series['day'], 
                                      production_time_series[liquid],bounds=(0, [qi,b_upper,di_bound]))
 
-        print('Hyperbolic Fit Curve-fitted Variables: qi='+str(popt_hyp[0])+', b='+str(popt_hyp[1])+', di='+str(popt_hyp[2]))
+        #print('Hyperbolic Fit Curve-fitted Variables: qi='+str(popt_hyp[0])+', b='+str(popt_hyp[1])+', di='+str(popt_hyp[2]))
 
         #Hyperbolic fit results
         production_time_series.loc[:,'hyperbolic_predicted']=hyperbolic_equation(production_time_series['day'], 
                                   *popt_hyp)
+        
+        y = production_time_series[liquid]
+        y_fit = production_time_series['hyperbolic_predicted']
+
+        # residual sum of squares
+        ss_res = np.sum((y - y_fit) ** 2)
+
+        # total sum of squares
+        ss_tot = np.sum((y - np.mean(y)) ** 2)
+
+        # r-squared
+        r2 = 1 - (ss_res / ss_tot)
+        
+        print('Hyperbolic Fit Curve-fitted Variables:')
+        print('r2='+str(round(r2, 4))+' qi='+str(popt_hyp[0])+', b='+str(popt_hyp[1])+', di='+str(popt_hyp[2]))
 
         #Declare the x- and y- variables that we want to plot against each other
         y_variables=[f'{liquid}', "hyperbolic_predicted"]
         x_variable='day'
 
         #Create the plot title
-        plot_title='liquid'+' Production for Well API '+str(api_number) 
+        plot_title='liquid'+' Production for Well API '+str(api_number)
 
         #Plot the data to visualize the equation fit
         plot_actual_vs_predicted_by_equations(production_time_series, x_variable, y_variables, plot_title)
+
