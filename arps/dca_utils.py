07dce19063b3e9c3a40ad4f4d7aff0050cb0c543
andrew allen <59720906+andrewjeffallen@users.noreply.github.com>
2021-01-16 09:20:57 -0700

    Update and rename dca.py to dca_utils.py

---
+++ arps/dca_utils.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+def hyperbolic_equation(t, qi, b, di):
+    return qi/((1.0+b*di*t)**(1.0/max(b, 1.e-50)))
+    
+def get_qi(df, days, var_col, date_col):
+    df=df.sort_values(by=var_col)
+    df_init_prod=df.head(days)
+    return df_init_prod[date_col].max()
+    
+def plot_dca(df, x_variable, y_variables, plot_title):
+    """
+    Plot Results of graphs
+    """
+    df.plot(x=x_variable, y=y_variables, title=plot_title)
+    plt.show()
+    
+
