fbd2d7214f1a77a8adcf8a34b077742d61e592a2
andrewjeffallen <andrewjeffallen@gmail.com>
2021-08-10 14:20:39 -0600

    pca models

--- arps/ml_prediction.py
+++ arps/ml_prediction.py
@@ -1,7 +1,12 @@
-# Simple comparison of LR vs MLP on cleaned time series datasets
-
 import sklearn.metrics as metrics
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.linear_model import LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import make_scorer
+from sklearn.preprocessing import StandardScaler
+
+# Simple comparison of LR vs MLP on cleaned time series datasets
 
 
 def regression_results(y_true, y_pred):
@@ -20,59 +25,29 @@ def regression_results(y_true, y_pred):
     print("RMSE: ", round(np.sqrt(mse), 4))
 
 
-time_series = pd.read_csv("daily_production.csv")
-time_series[time_series.API == 42493326380000]
-time_series = time_series.astype({"API": str, "OIL (BBL)": float})
-time_series = time_series[
-    (time_series["OIL (BBL)"].notnull()) & (time_series["OIL (BBL)"] > 0)
-]
-
-data = time_series[["D_DATE", "OIL (BBL)"]]
-
-data["D_DATE"] = pd.to_datetime(data["D_DATE"])
-data = data.set_index("D_DATE")
-
-
-data_consumption = data[["OIL (BBL)"]]
-# inserting new column with yesterday's consumption values
-data_consumption.loc[:, "Yesterday"] = data_consumption.loc[:, "OIL (BBL)"].shift()
-# inserting another column with difference between yesterday and day before yesterday's consumption values.
-data_consumption.loc[:, "Yesterday_Diff"] = data_consumption.loc[:, "Yesterday"].diff()
-# dropping NAs
-data_consumption = data_consumption.dropna()
-
-
-X_train = data_consumption[data_consumption.index < "2017"].drop(["OIL (BBL)"], axis=1)
-y_train = data_consumption[data_consumption.index < "2017"]["OIL (BBL)"]
-
-
-X_test = data_consumption[data_consumption.index >= "2017"].drop(["OIL (BBL)"], axis=1)
-y_test = data_consumption[data_consumption.index >= "2017"]["OIL (BBL)"]
-
 # Spot Check Algorithms
-from sklearn.linear_model import LinearRegression
-from sklearn.neural_network import MLPRegressor
-from sklearn.model_selection import cross_val_score
 
-models = []
-models.append(("LR", LinearRegression()))
-models.append(("NN", MLPRegressor(solver="lbfgs", max_iter=10000)))  # neural network
-# Evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-    tscv = TimeSeriesSplit(n_splits=10)
-    cv_results = cross_val_score(model, X_train, y_train, cv=tscv, scoring="r2")
-    results.append(cv_results)
-    names.append(name)
-    print("%s: %f (%f)" % (name, cv_results.mean(), cv_results.std()))
 
-# Compare Algorithms
-plt.boxplot(results, labels=names)
-plt.title("Algorithm Comparison")
-plt.show()
+def compare_algorithms(x_train, y_train):
+    models = []
+    models.append(("LR", LinearRegression()))
+    models.append(
+        ("NN", MLPRegressor(solver="lbfgs", max_iter=10000))
+    )  # neural network
+    # Evaluate each model in turn
+    results = []
+    names = []
+    for name, model in models:
+        tscv = TimeSeriesSplit(n_splits=10)
+        cv_results = cross_val_score(model, X_train, y_train, cv=tscv, scoring="r2")
+        results.append(cv_results)
+        names.append(name)
+        print("%s: %f (%f)" % (name, cv_results.mean(), cv_results.std()))
 
-from sklearn.metrics import make_scorer
+    # Compare Algorithms
+    plt.boxplot(results, labels=names)
+    plt.title("Algorithm Comparison")
+    plt.show()
 
 
 def rmse(actual, predict):
@@ -85,17 +60,62 @@ def rmse(actual, predict):
     return score
 
 
-mlp_model = MLPRegressor(solver="lbfgs", max_iter=10000)
-mlp_model.fit(X_test, y_test)
+def fit_model(model, X_test, y_test):
 
-mlp_y_true = y_test.values
-mlp_y_pred = mlp_model.predict(X_test)
-regression_results(mlp_y_true, mlp_y_pred)
+    if model == "MLP":
+        mlp_model = MLPRegressor(solver="lbfgs", max_iter=10000)
+        mlp_model.fit(X_test, y_test)
+
+        mlp_y_true = y_test.values
+        mlp_y_pred = mlp_model.predict(X_test)
+        return regression_results(mlp_y_true, mlp_y_pred)
+
+    elif model == "LR":
+        lr_model = LinearRegression()
+        lr_model.fit(X_test, y_test)
+
+        lr_y_true = y_test.values
+        lr_y_pred = lr_model.predict(X_test)
+        return regression_results(lr_y_true, lr_y_pred)
 
 
-lr_model = LinearRegression()
-lr_model.fit(X_test, y_test)
+def create_pca_df(data, dataframe, num_pc, time_horizon, unique_key, period):
+    """
+    data: well parameters pandas dataframe
+    dataframe: time series pandas dataframe
+    num_pc: the number of principal components
+    time_horizon: int, time horizon to extract features from
+    unique_key =
+    """
+    df_all["month_online"] = df_all.groupby(by=unique_key).cumcount() + 1
+    df = df_all.groupby(by="API").head(time_horizon)
+    df_wide = df.pivot(index="API", columns="month_online", values="Oil_MA")
+    df_wide = df_wide.dropna()
+    features = df_wide.columns[:num_pc]
 
-lr_y_true = y_test.values
-lr_y_pred = lr_model.predict(X_test)
-regression_results(lr_y_true, lr_y_pred)
+    x = df_wide.loc[:, features].values
+
+    x = StandardScaler().fit_transform(x)
+
+
+    pc_cols = ", ".join([f"PC{i}" for i in range(1, num_pc + 1)])
+
+    principalDf = pd.DataFrame(
+        data=principalComponents, columns=[f"{pc_cols}"], index=df_wide.index
+    )
+    df_test = principalDf.merge(data, left_index=True, right_on="API")
+
+    pcs = PCA()
+    pcs.fit(preprocessing.scale(df_wide.loc[:, features].dropna(axis=0)))
+    pcsSummary_df = pd.DataFrame(
+        {
+            "Standard deviation": np.sqrt(pcs.explained_variance_),
+            "Proportion of variance": pcs.explained_variance_ratio_,
+            "Cumulative proportion": np.cumsum(pcs.explained_variance_ratio_),
+        }
+    )
+    pcsSummary_df = pcsSummary_df.transpose()
+    pcsSummary_df.columns = [
+        "PC{}".format(i) for i in range(1, len(pcsSummary_df.columns) + 1)
+    ]
+    pcsSummary_df.round(3)
